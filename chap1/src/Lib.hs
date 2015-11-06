bbba96b302dcce7f25ffe66b3f1674d948a879f1
Bryan Feeney <bryan.feeney@gmail.com>
2015-11-06 19:02:59 +0000

    Initial Chapter 1 Stub application, missing unit-tests & benchmarks
    Has the basic Location type. Currently missing unit-tests and benchmark
    tests, and documentation tests.

---
+++ chap1/src/Lib.hs
@@ -0,0 +1,10 @@
+module Lib (
+   module VForth.Location
+ , welcomeMsg
+ ) where
+
+import VForth.Location
+
+-- | The message shown when the application starts
+welcomeMsg :: String
+welcomeMsg = "Wake up! It's time to venture forth"
